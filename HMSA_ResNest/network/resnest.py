5547e8507bce9b73b78cf243baf95b3a8bb63631
Brian0615 <brianps.lam@mail.utoronto.ca>
2021-04-17 14:44:32 -0400

    add combined models

---
+++ HMSA_ResNest/network/resnest.py
@@ -0,0 +1,54 @@
+##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Created by: Hang Zhang
+## Email: zhanghang0704@gmail.com
+## Copyright (c) 2020
+##
+## LICENSE file in the root directory of this source tree
+##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+"""ResNeSt models"""
+
+import torch
+from network.Resnet import ResNet, Bottleneck
+import torch.utils.model_zoo as model_zoo
+
+__all__ = ['resnest50', 'resnest101', 'resnest200', 'resnest269']
+
+_url_format = 'https://hangzh.s3.amazonaws.com/encoding/models/{}-{}.pth'
+
+
+def resnest50(pretrained=False, **kwargs):
+    model = ResNet(Bottleneck, [3, 4, 6, 3],
+                   radix=2, groups=1, bottleneck_width=64,
+                   deep_stem=True, stem_width=32, avg_down=True,
+                   avd=True, avd_first=False, **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(_url_format.format('resnest50', 'fb9de5b3')), strict=True)
+    return model
+
+def resnest101(pretrained=False, **kwargs):
+    model = ResNet(Bottleneck, [3, 4, 23, 3],
+                   radix=2, groups=1, bottleneck_width=64,
+                   deep_stem=True, stem_width=64, avg_down=True,
+                   avd=True, avd_first=False, **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(_url_format.format('resnest101', '966fb78c')), strict=True)
+    return model
+
+def resnest200(pretrained=False, **kwargs):
+    model = ResNet(Bottleneck, [3, 24, 36, 3],
+                   radix=2, groups=1, bottleneck_width=64,
+                   deep_stem=True, stem_width=64, avg_down=True,
+                   avd=True, avd_first=False, **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(_url_format.format('resnest200', 'd7fd712f')), strict=False)
+    return model
+
+def resnest269(pretrained=False, **kwargs):
+    model = ResNet(Bottleneck, [3, 30, 48, 8],
+                   radix=2, groups=1, bottleneck_width=64,
+                   deep_stem=True, stem_width=64, avg_down=True,
+                   avd=True, avd_first=False, **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(_url_format.format('resnest269', '51ae5f19')), strict=True)
+    return model
+
